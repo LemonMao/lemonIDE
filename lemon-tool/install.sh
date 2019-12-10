101dc16854f782f0a386fee93eb2835751610105
Lemon Mao <shmao@sonicwall.com>
2019-12-10 17:05:39 +0800

    Remove installing Plug.vim with curl

--- lemon-tool/install.sh
+++ lemon-tool/install.sh
@@ -24,8 +24,8 @@ ln -s ~/.vim/vimrc $vimrc
 mkdir -v $dstPluginPath/
 #git clone https://github.com/gmarik/vundle.git $dstPluginPath/vundle/
 #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-[ $? -ne 0 ] && echo "!!!!  Install vindle tool failed !" && exit
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#[ $? -ne 0 ] && echo "!!!!  Install vindle tool failed !" && exit
 echo " "
 #cp $srcPluginPath/vundle $dstPluginPath/vundle
 #cp ../bundle ~/.vim/ -rf
