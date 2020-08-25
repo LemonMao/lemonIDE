519b95cde1e5b920d4675dd5495d0c1669dd1e75
Lemon Mao <shmao@sonicwall.com>
2020-08-25 03:17:24 +0000

    Update vim7 configuration.

--- lemon-tool/install_v7.sh
+++ lemon-tool/install_v7.sh
@@ -17,6 +17,30 @@ rm -rf $dstPluginPath/gutentags_plus
 rm -rf $dstPluginPath/vim-gutentags
 rm -rf $dstPluginPath/vim-preview
 rm ~/.vimrc
+
+###########  Customize private plugins 
+# my theme
+cp $srcPluginPath/desertmss.vim $dstPluginPath/desertEx/colors/
+# tmux config
+[ -e ~/.tmux.conf ] && mv ~/.tmux.conf $vimbak/tmux.conf-bak
+ln -s $srcPluginPath/tmux.conf ~/.tmux.conf
+# bash alias
+#[ -e ~/.bash_aliases ] && cat $srcPluginPath/bash_aliases >> ~/.bash_aliases || ln -s $srcPluginPath/bash_aliases ~/.bash_aliases
+read -e -p "Shell alias config, additional apply[Y/n]:" ret
+if [[ $ret != "n" ]] ; then
+    cat $srcPluginPath/lemon_bashrc >> ~/.bashrc
+fi
+# Bookmark
+[ -e ~/.NERDTreeBookmarks ] && mv ~/.NERDTreeBookmarks $vimbak/NERDTreeBookmarks-bak
+ln -s $srcPluginPath/NERDTreeBookmarks ~/.NERDTreeBookmarks
+# c&sh snippets.
+[ -e $dstPluginPath/snipMate/snippets/c.snippets ] && mv $dstPluginPath/snipMate/snippets/c.snippets $vimbak/
+[ -e $dstPluginPath/snipMate/snippets/sh.snippets ] && mv $dstPluginPath/snipMate/snippets/sh.snippets $vimbak/
+ln -s $srcPluginPath/c.snippets  $dstPluginPath/snipMate/snippets/c.snippets 
+ln -s $srcPluginPath/sh.snippets $dstPluginPath/snipMate/snippets/sh.snippets
+#[ -e  ] && mv 
+
+
 ln -s ~/.vim/vimrc_v7 ~/.vimrc 
 vim +PlugInstall +qall
 
